85a7d6bb60b33a4460dc5c9bfc1bba793584162c
Nycz-lab <Skybreak00@protonmail.com>
2025-04-20 01:57:42 +0200

    remove generated file from tracking

--- src/lucyt_version.h
+++
@@ -1,6 +0,0 @@
-#ifndef LUCYT_VERSION_H_
-#define LUCYT_VERSION_H_
-#define LUCYT_VERSION "v0.0.0-unknown"
-#define LUCYT_AUTHOR "Nycz"
-#define LUCYT_NAME "Lucyt-Engine"
-#endif  /* LUCYT_VERSION_H_ */
